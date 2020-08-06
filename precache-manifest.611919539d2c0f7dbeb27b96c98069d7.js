51b052873925cd22b7c3dbefa54e0e707e96404e
hudhanst <60328630+hudhanst@users.noreply.github.com>
2020-08-06 07:10:19 +0700

    testing using build

---
+++ precache-manifest.611919539d2c0f7dbeb27b96c98069d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f747dfe008a5f662c49115c411f6b30",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aaa2d4f345e3dcd564d8",
+    "url": "./static/css/main.d6a48e5c.chunk.css"
+  },
+  {
+    "revision": "571d12a702ee839e7157",
+    "url": "./static/js/2.a0c41df3.chunk.js"
+  },
+  {
+    "revision": "aa0d51ee197b7c16d4e47e6d3cdadd26",
+    "url": "./static/js/2.a0c41df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaa2d4f345e3dcd564d8",
+    "url": "./static/js/main.87dd4aa5.chunk.js"
+  },
+  {
+    "revision": "8d73c22166bfb0401e91",
+    "url": "./static/js/runtime-main.b3fc422b.js"
+  }
+]);
